89575f98cb351d117edb37df2ef9c459fc810182
Felipe Santos <fdms@icomp.ufam.edu.br>
2020-04-01 19:07:42 -0400

    comentando server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 const app = require('./app')
 const port = 3333
 
+//Exclusive to make the backend run on the port regardless of the test script
 console.log('[VIRTUAL STORE BACKEND]')
-
-
 app.listen(port)
 console.log('[HTTP] Port:', port)
