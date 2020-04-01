ec7506d227ee7be52853dcca8db89183b06e957d
Felipe Santos <fdms@icomp.ufam.edu.br>
2020-04-01 17:42:47 -0400

    projeto finalizado

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 #**Projeto Loja Virtual**#
 
 
+Consiste em uma aplicação web onde há um CRUD de um recurso chamado produto, toda a interação do usuário com o banco de dados é possível ser feita através de um frontend web.
+
+
 **Tecnologias Utilizadas**
 
 *	Para o desenvolvimento do backend foi utilizada a plataforma Node.js baseada na linguagem Javascript
 *	Para o desenvolvimento do frontend foi utilizada a plataforma React.js baseado na linguagem Javascript
-* 	O banco de dados utilizado foi SQLite
+* 	O banco de dados utilizado foi SQLite, por não necessitar de um processo SGBD sendo executado separadamente
+*	Testes foram feitos no backend através do framework Jest 
 
 
 
@@ -13,6 +17,7 @@
 
 *	Node 10.x ou superior
 *	NPM 6.x ou superior
+*	Yarn 1.X ou superior
 
 
 **Executando Backend:**
@@ -25,7 +30,24 @@
 
 		npm start
 
+**Testes no Backend:**
+
+1. Para verificar o script de testes que foram feitos no desenvolvimento da aplicação acessar o diretório ./backend/ e executar o comando abaixo:
+
+		npm test
+
+
 **Executando Frontend:**
+
+Com o backend sendo executado, pode-se executar os passos abaixo para rodar o frontend da aplicação localmente.
+
+1. Acessar o diretório ./frontend/ e executar o comando abaixo para instalar as dependências:
+
+		yarn install
+
+2. Uma vez instaladas as dependências, pelo comando abaixo é possível executar o backend da aplicação:
+
+		yarn start
 
 
 
--- backend/tests/product.spec.js
+++ backend/tests/product.spec.js
@@ -17,43 +17,145 @@ describe('PRODUCT', () => {
                 .post('/products')
                 .send({        
                     title: "Produto Teste",
-                    description: "Esta é uma descrição teste do produto",
+                    description: "Esta é uma descrição teste do produto.",
                     value: "10"
                 })
 
-        console.log(response.body)
-
+        
         expect(response.body).toHaveProperty('id')
     })
 
-    it('shouldn\'t be able to create a new product with string value', async () => {
+    it('should be able to create a new product', async () => {
         const response = await request(app)
                 .post('/products')
                 .send({        
                     title: "Produto Teste",
-                    description: "Esta é uma descrição teste do produto",
+                    description: "Esta é uma descrição teste do produto.",
+                    value: 100
+                })
+
+        
+        expect(response.body).toHaveProperty('id')
+    })
+
+    it('shouldn\'t be able to create a new product with params with invalid data type', async () => {
+        const response = await request(app)
+                .post('/products')
+                .send({        
+                    title: 12,
+                    description: 3.4,
                     value: "string"
                 })
                 .expect(400)
-
-        console.log(response.body)
-
-        
     })
 
     it('shouldn\'t be able to create a new product with missing params', async () => {
         const response = await request(app)
                 .post('/products')
+                .send({})
+                .expect(400)  
+    })
+
+    it('should be able to get all products info at database', async () => {
+        const response = await request(app)
+                .get('/products')
+        
+        expect(response.body)
+    })
+
+    it('should be able to get an specific product info at database', async () => {
+        
+        const response = await request(app)
+                .post('/products')
                 .send({        
                     title: "Produto Teste",
-                    description: "Esta é uma descrição teste do produto"
-
+                    description: "Esta é uma descrição teste do produto.",
+                    value: "10"
                 })
-                .expect(400)
 
-        console.log(response.body)
-
+        const test = await request(app)
+                .get('/product/' + response.body.id)
         
+        expect(test.body).toHaveProperty('id')
     })
+
+    it('should\'t be able to get an specific product with invalid param', async () => {
+        
+        const test = await request(app)
+                .get('/product/test' )
+        
+        expect(400)
+    })
+
+    it('should be able to edit a existing product', async () => {
+        
+        const response = await request(app)
+                .post('/products')
+                .send({        
+                    title: "Produto Teste",
+                    description: "Esta é uma descrição teste do produto.",
+                    value: "10"
+                })
+
+
+        const put = await request(app)
+                .put('/products/' + response.body.id)
+                .send({        
+                    title: "Produto Teste Alterado",
+                    description: "Esta é uma descrição teste do produto alterado.",
+                    value: "15"
+                })
+
+        const get = await request(app)
+                .get('/product/' + response.body.id)
+                .send({        
+                    title: "Produto Teste Alterado",
+                    description: "Esta é uma descrição teste do produto alterado.",
+                    value: "15"
+                })
+
+        expect({
+            title: get.body.title,
+            description: get.body.description,
+            value: get.body.value
+        }).toEqual({
+            title: "Produto Teste Alterado",
+            description: "Esta é uma descrição teste do produto alterado.",
+            value: 15
+        })
+    })
+
+    it('should\'t be able to edit an specific product with invalid param', async () => {
+        
+        const test = await request(app)
+                .put('/products/test' )
+        
+        expect(400)
+    })
+
+    it('should be able to delete an specific product at database', async () => {
+        
+        const response = await request(app)
+                .post('/products')
+                .send({        
+                    title: "Produto Teste",
+                    description: "Esta é uma descrição teste do produto.",
+                    value: "10"
+                })
+
+        const test = await request(app)
+                .delete('/products/' + response.body.id)
+        
+        expect(204)
+    })
+
+    it('should\'t be able to delete an specific product with invalid param', async () => {
+        
+        const test = await request(app)
+                .delete('/products/test' )
+        
+        expect(400)
+    })
+
 
 })
